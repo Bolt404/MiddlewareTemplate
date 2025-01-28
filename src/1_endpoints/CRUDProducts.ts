b7eca2f17d8f375d6a28f901d59d20b78a3c38cd
Mads Knudsen <mads@rknudsen.dk>
2025-01-28 09:21:24 +0100

    save

--- src/1_endpoints/CRUDProducts.ts
+++ src/1_endpoints/CRUDProducts.ts
@@ -4,7 +4,7 @@ class CRUDProduct{
    // En mockup af en database (in memory) - se REDIS senere
    private static products: Map<number, IProduct> = new Map<number, IProduct>();
    
-   private static size: number =0;
+   private static size: number = 0;
 
    // CRUD Create
    public static async insert(request:any, response:any){
